a1726d6b5a06e6e3d8f924b0b689fec1a0c85508
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-08-08 13:36:19 -0700

    First Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Apple on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
